4ea00eac743b10f1bb7723e52559135a2000e9a3
yongmon01 <60520539+yongmon01@users.noreply.github.com>
2021-01-05 22:58:59 +0900

    learned view and api

--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -3,12 +3,48 @@ package hello.hellospring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
     @GetMapping("hello") //      /hello에 들어왔을때 호출된다.
-    public String hello(Model model){
+    public String hello(Model model) {
         model.addAttribute("data", "hello!!");
-        return "hello";
+        return "hello"; //     /hello 로 data를 넘겨준다
     }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam("enterName") String name, Model model) {
+        model.addAttribute("enterName", name);
+        return "hello-template";
+    }
+
+    //
+    @GetMapping("hello-string")
+    @ResponseBody
+    public String helloString(@RequestParam("name") String name){
+        return "hi " + name;
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name){
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    }
+
+    static class Hello{
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
+
 }
